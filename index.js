171f7fa198854b74a49ff3a0e62c64609847d7dd
Shamim <yeasinshamim7@gmail.com>
2022-05-19 03:29:31 +0600

    project done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -19,12 +19,25 @@ async function run() {
         await client.connect();
         const todoCollection = client.db("todoList").collection("task");
 
+        app.get('/todoTask', async (req, res) => {
+            const task = await todoCollection.find({}).toArray();
+            res.send(task);
+        });
+
         app.post('/todoTask', async (req, res) => {
             const task = req.body;
             const doc = task;
             const result = await todoCollection.insertOne(doc);
             res.send(result)
-        })
+        });
+
+        app.delete('/todoTask/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await todoCollection.deleteOne(query);
+            res.send(result);
+        });
+
     }
     finally { }
 }
