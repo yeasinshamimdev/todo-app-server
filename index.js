d9b8d9adae988685f4d5ed401b57499a2ad4f365
Shamim <yeasinshamim7@gmail.com>
2022-05-19 02:18:56 +0600

    mongodb connet

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -8,6 +9,27 @@ const app = express();
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7qpfm.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const todoCollection = client.db("todoList").collection("task");
+
+        app.post('/todoTask', async (req, res) => {
+            const task = req.body;
+            const doc = task;
+            const result = await todoCollection.insertOne(doc);
+            res.send(result)
+        })
+    }
+    finally { }
+}
+run().catch(console.dir);
+
 
 app.get('/', async (req, res) => {
     res.send('Hello from todo app');
